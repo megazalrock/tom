00cdda2f5ae905cb9b92adcd68ce0c8e33e0c4da
otto <otto@mgzl.jp>
2014-08-15 22:58:27 +0900

    fix

--- main.js
+++ main.js
@@ -1,70 +1,68 @@
 (function(window, $, angular, undefined){
 	"use strict";
 	var towerOfMetabApp = angular.module('towerOfMetabApp', ['ngStorage']);
-	towerOfMetabApp.controller('MainCtrl', ['$scope', '$localStorage', '$sessionStorage', function($scope, $localStorage, $sessionStorage){
+	towerOfMetabApp.controller('MainCtrl', ['$scope', '$http', '$q', '$localStorage', '$sessionStorage', function($scope, $http, $q, $localStorage, $sessionStorage){
 
-		//$scope.targetUrl = 'about:blank';
+		$scope.isLoading = false;
 		$scope.$strage = $localStorage;
-		$scope.targetUrl = $scope.$strage .targetUrl || 'http://headlines.yahoo.co.jp/hl?a=20140812-00000026-at_s-l22';
+		$scope.targetUrl = $scope.$strage.targetUrl || 'about:blank';
 		$scope.bookmarksPages = [];
 		if(!angular.isUndefined($scope.$strage.bookmarksPages)){
 			$scope.bookmarksPages = JSON.parse($scope.$strage.bookmarksPages);
 		}
-		$scope.bookmarksSum = $scope.$strage.bookmarksSum;
+		$scope.bookmarksSum = $scope.$strage.bookmarksSum || 0;
+		$scope.isLoadingClass = 'hidden';
+		$scope.isNotLoadingClass = '';
 
 		$scope.climb = function(){
+			if($scope.isLoading){
+				return false;
+			}
+
 			$scope.bookmarksPages = [];
 			$scope.bookmarksSum = 0;
+			$scope.$strage.targetUrl = $scope.targetUrl;
+			$scope.isLoading = true;
+			$scope.isLoadingClass = '';
+			$scope.isNotLoadingClass = 'hidden';
 			(function loop(url){
-				$scope.$strage.targetUrl = $scope.targetUrl;
-				getHateb(url)
+				$http
+					.jsonp('http://api.b.st-hatena.com/entry/jsonlite/', {
+						params: {
+							url: url,
+							callback: 'JSON_CALLBACK'
+						}
+					})
 					.then(function(res){
-						if(res !== null && $scope.bookmarksPages.length < 12){
-							$scope.$apply(function(){
-								$scope.bookmarksPages.unshift(res);
-								$scope.bookmarksSum += parseFloat(res.count);
-							});
-							loop(res.entry_url);
+						var data = res.data;
+						if(data !== null && $scope.bookmarksPages.length < 12){
+							$scope.bookmarksPages.unshift(data);
+							$scope.bookmarksSum += parseFloat(data.count);
+							loop(data.entry_url);
 						}else{
-							$scope.$apply(function(){
-								$scope.$strage.bookmarksPages = JSON.stringify($scope.bookmarksPages);
-								$scope.$strage.bookmarksSum = $scope.bookmarksSum;
-							});
+							$scope.$strage.bookmarksPages = JSON.stringify($scope.bookmarksPages);
+							$scope.$strage.bookmarksSum = $scope.bookmarksSum;
+							$scope.isLoading = false;
+							$scope.isLoadingClass = 'hidden';
+							$scope.isNotLoadingClass = '';
 						}
 					});
 			})($scope.targetUrl);
-			function getHateb(url){
-				var deferred = $.Deferred();
-				if(typeof url !== 'string'){
-					url = url.entry_url;
-				}
-				$.ajax('http://api.b.st-hatena.com/entry/jsonlite/',{
-					type: 'GET',
-					dataType: 'jsonp',
-					data: {
-						url: url,
-						callback: 'callback'
-					},
-					jsonp: true,
-					jsonpCallback: 'callback'
-				})
-				.then(function(res){
-					deferred.resolve(res);
-				});
-				return deferred.promise();
-			}
 		};
 		$scope.getDate = function(datetime){
 			return datetime.match(/\d+\/\d+\/\d+/)[0].replace(/\//g, '');
 		};
 		$scope.hasIdCallClass = function(comment){
 			var classes = [];
-			if((/id:[A-Za-z0-9]+?\s/.test(comment))){
+			if((/id:[\w-]+/.test(comment))){
 				classes = ['hasIdCall'];
 			}
 			return classes;
 		};
+		$scope.keyup = function(e){
+			if(e.keyCode === 13){
+				$scope.climb();
+			}
+		};
 	}]);
-
-
 })(this, jQuery, angular);
